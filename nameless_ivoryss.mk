23a004423e2c7b555283fbc96914158d9b70774c
Alonso1398 <chinomat100@gmail.com>
2015-03-21 14:08:27 -0600

    update ramdisk remove zram, it performs better without it also added prebuilt charger binary from cm 10.1 to fix offline charging and remove init.bt.rc because it isn't necessary

--- nameless_ivoryss.mk
+++ nameless_ivoryss.mk
@@ -43,12 +43,11 @@ PRODUCT_COPY_FILES += \
         device/samsung/ivoryss/rootdir/init.rhea_ss_ivoryss.rc:root/init.rhea_ss_ivoryss.rc \
         device/samsung/ivoryss/rootdir/init.bcm2165x.usb.rc:root/init.bcm2165x.usb.rc \
         device/samsung/ivoryss/rootdir/init.log.rc:root/init.log.rc \
-	device/samsung/ivoryss/rootdir/init.bt.rc:root/init.bt.rc \
         device/samsung/ivoryss/rootdir/lpm.rc:root/lpm.rc \
         device/samsung/ivoryss/rootdir/ueventd.rhea_ss_ivoryss.rc:root/ueventd.rhea_ss_ivoryss.rc \
         device/samsung/ivoryss/rootdir/init.recovery.rhea_ss_ivoryss.rc:root/init.recovery.rhea_ss_ivoryss.rc \
         device/samsung/ivoryss/rootdir/fstab.rhea_ss_ivoryss:root/fstab.rhea_ss_ivoryss \
-        device/samsung/ivoryss/rootdir/init.zram.sh:root/init.zram.sh
+        device/samsung/ivoryss/rootdir/charger:root/charger
 
 # Prebuilt kl keymaps
 PRODUCT_COPY_FILES += \
