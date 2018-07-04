915aba45e0c02fd6ada14c3ec14af8c032476585
Luis Alonso <luis_al_1398@hotmail.com>
2018-07-03 23:49:54 -0600

    correct name device folder for device.nk
    Was set to corsica instead of ivoryss

--- full_ivoryss.mk
+++ full_ivoryss.mk
@@ -24,7 +24,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from corsica device
-$(call inherit-product, device/samsung/corsica/device.mk)
+$(call inherit-product, device/samsung/ivoryss/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_ivoryss
